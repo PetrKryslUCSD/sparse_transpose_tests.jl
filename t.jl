62bcdbcc6e1eb8c31a7ad22a9c01c1875e5be272
Petr Krysl <pkrysl@ucsd.edu>
2026-03-19 05:38:37 -0700

    try to force csr

--- t.jl
+++ t.jl
@@ -7,6 +7,7 @@ include("csr_sparse_transpose.jl")
 ################################################################################
 ################################################################################
 ################################################################################
+
 M = 2 * 10_000 + 133
 N = M
 sparsity = 10  / M
@@ -14,14 +15,21 @@ sparsity = 10  / M
 Ac = sprand(M, N, sparsity);
 @info "Matrix M = $M by N = $N, sparsity = $sparsity, nnz = $(nnz(Ac))"
 Ar = copy(sparsecsr(Ac));
+@show typeof(Ac), typeof(Ar)
 
-check = false
+check = !false
 
 Gc = GBMatrix(Ac); # GraphBLAS sparse CSC copy
+# setstorageorder!(Gc, ColMajor())
+gbset(Gc, :format, :bycol)
 Gr = GBMatrix(copy(sparsecsr(Ac))); # GraphBLAS sparse CSR copy
+# setstorageorder!(Gr, RowMajor())
+gbset(Gr, :format, :byrow)
+@show typeof(Gc), typeof(Gr)
 
 if check
     @show norm(gbtranspose(Gc) - copy(transpose(Ac))) # check
+    @show norm(gbtranspose(Gr) - copy(transpose(Ar))) # check
 end
 
 @info "Benchmarking copy+transpose CSC"
